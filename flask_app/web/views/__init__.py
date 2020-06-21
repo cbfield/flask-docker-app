7f5307848e28f49f8a8b611796de11dc201f4c09
Christopher Basketfield <chris.basketfield@gmail.com>
2020-06-21 01:42:48 -0800

    submodule organization

--- flask_app/web/views/__init__.py
+++ flask_app/web/views/__init__.py
@@ -1,10 +1,11 @@
 import os
 
 modules = [
-    f for f in os.listdir(os.path.dirname(os.path.abspath(__file__)))
-    if f.endswith('.py') and f != '__init__.py']
+    f[:-3] for f in os.listdir(os.path.dirname(os.path.abspath(__file__)))
+    if f.endswith('.py') and f != '__init__.py'
+]
 
 blueprints = []
 for module in modules:
-    mod = __import__(name=f'web.views.{module[:-3]}',fromlist=['views'])
+    mod = __import__(name = f'{__name__}.{module}', fromlist = ['views'])
     blueprints.append(mod.views)
