034e32fc842e83eb85343fea134b40e39ae1e1be
Christopher Basketfield <chris.basketfield@gmail.com>
2020-06-21 00:02:01 -0800

    better string trimming

--- web/views/__init__.py
+++ web/views/__init__.py
@@ -9,7 +9,7 @@ for module in modules:
     if (not module.endswith('.py')) or (module == '__init__.py'):
         continue
 
-    spec = importlib.util.spec_from_file_location(f'{module.replace(".py","")}', f'{pwd}/{module}')
+    spec = importlib.util.spec_from_file_location(f'{module[:-3]}', f'{pwd}/{module}')
     mod = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(mod)
     
